52c853b49a3dbbe60ec8c49da1a11b68b39af1b2
jeferson carvalho <carvalho.jeferson@gmail.com>
2020-03-24 15:19:46 -0300

    corrigi o texto informativo

--- repos/ExercicioConjuntos/ExercicioConjuntos/Program.cs
+++ repos/ExercicioConjuntos/ExercicioConjuntos/Program.cs
@@ -8,15 +8,10 @@ namespace ExercicioConjuntos
         static void Main(string[] args)
         {
             /*
-             No portal de cursos online Udemy, cada usuário possui um código único, representado por um número inteiro.
-Cada instrutor do portal Udemy pode ter vários cursos, sendo que um mesmo aluno pode se matricular em quantos
-cursos quiser. Assim, o número total de alunos de um instrutor não é simplesmente a soma dos alunos de todos os
-cursos que ele possui, pois pode haver alunos repetidos em mais de um curso.
-O instrutor Joaquim possui três cursos A, B e C, e deseja saber seu número total de alunos.
-Seu programa deve ler os alunos dos cursos A, B e C do instrutor Joaquim, depois mostrar a quantidade total e alunos
-dele, conforme exemplo.
-Veja o exemplo (os valores que devem ser digitados pelo usuário estão destacados em vermelho):
-             */
+             Um instrutor possui três cursos: A, B e C, e deseja saber seu 
+             número total de alunos. Seu programa deve ler os alunos dos 
+             cursos A, B e C do instrutor Joaquim, depois mostrar a 
+             quantidade total e alunos dele,  */
             HashSet<int> A = new HashSet<int>();
             HashSet<int> B = new HashSet<int>();
             HashSet<int> C = new HashSet<int>();
